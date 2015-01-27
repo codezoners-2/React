26b6641e049e13b890294bed241e610e046bd287
Nick Rothwell <nick@yedikule.clients.rave.ac.uk>
2015-01-27 12:10:09 +0000

    Tweaks to functions.

--- 03_grids_and_calculation_with_React/assignments/functions/HANDOUT/js/functions.js
+++ 03_grids_and_calculation_with_React/assignments/functions/HANDOUT/js/functions.js
@@ -34,7 +34,8 @@ function makeRed(n) {
 // Range: 1000
 function sinMonoFn(n) {
     var s = Math.sin(n * Math.PI * 2 / 1000);
-    return <MyBox r="255" g="80" b="0" width="1" height={40 + s * 40} />;
+
+    return <MyBox r="255" g="128" b="0" width="1" height={40 * (s + 1)} />;
 }
 
 var Block = React.createClass({
